844e01cb82f86e8c8c5758a6d63c76c5a028092a
Lex <37273428+lexiddie@users.noreply.github.com>
2021-03-23 22:10:45 +0700

    migrate project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { withRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-function App() {
+import Home from '../src/pages/home/home.component';
+import Main from '../src/pages/main/main.component';
+import Category from '../src/pages/category/category.component';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectIsSignIn } from './redux/user/user.selectors';
+
+import Header from './components/header/header.component';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import './sass/index.scss';
+
+const App = (props) => {
+  const { isSignIn, setCurrentUser, history } = props;
+
+  const checkSignIn = () => {
+    if (isSignIn) {
+      history.push('/main');
+    }
+  };
+
+  useEffect(() => {
+    checkSignIn();
+
+    const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+    });
+
+    return () => {
+      unsubscribeFromAuth();
+    };
+  }, [isSignIn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <Switch>
+        <Route exact path='/' render={() => (!isSignIn ? <Redirect from='*' to='/home' /> : null)} />
+        <Route path='/home' component={Home} />
+        <Route path='/main' component={Main} />
+        <Route path='/category' component={Category} />
+      </Switch>
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+  isSignIn: selectIsSignIn
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
